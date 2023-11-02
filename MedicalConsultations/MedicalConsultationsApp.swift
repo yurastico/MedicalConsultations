23b30cec34e6d2cbd8403c672f239c810a58eb16
Yuri Cunha <yurastico@Yuris-Mac-mini.local>
2023-11-01 23:41:33 -0300

    setup project

---
+++ MedicalConsultations/MedicalConsultationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedicalConsultationsApp.swift
+//  MedicalConsultations
+//
+//  Created by Yuri Cunha on 01/11/23.
+//
+
+import SwiftUI
+
+@main
+struct MedicalConsultationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
