e988b90591e0abac70d71413705ec6dc4947522f
Yuri Cunha <cunha.ysc@gmail.com>
2023-12-10 17:25:46 -0300

    starting a network layer

---
+++ MedicalConsultations/Networking/Base/Endpoint.swift
@@ -0,0 +1,27 @@
+//
+//  Endpoint.swift
+//  MedicalConsultations
+//
+//  Created by Yuri Cunha on 10/12/23.
+//
+
+import Foundation
+
+protocol Endpoint {
+    var schema: String { get }
+    var host: String { get }
+    var path: String { get }
+    var method: RequestMethod { get }
+    var header: [String: String] { get }
+    var body: [String: String] { get }
+}
+
+extension Endpoint {
+    var schema: String {
+        "http"
+    }
+    
+    var host: String {
+        "localhost"
+    }
+}
